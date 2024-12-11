f362a7576075d72c6fa8b577357d640b608035ba
shravnisingh <shravanisingh22@gmail.com>
2024-12-12 01:59:55 +0530

    UC11

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        IComputeEmpWage empWageBuilder = new EmpWageBuilder();
 
         System.out.println("Welcome to Employee Wage Computation Program!");
         System.out.print("Enter the number of companies: ");
