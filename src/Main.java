9d0d7abd29eeb148fd4ed2e69b96a2c88589d1e7
shravnisingh <shravanisingh22@gmail.com>
2024-12-11 23:28:16 +0530

    Main push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        }
+        System.out.println("Welcome to Employee Wage Computation");
     }
+}
