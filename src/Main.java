f9287a798e616f8ce5356adf0fd90486e6ee3349
shravnisingh <shravanisingh22@gmail.com>
2024-12-12 01:50:42 +0530

    UC09

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<EmpWageBuilder> companies = new ArrayList<>();
 
         System.out.println("Welcome to Employee Wage Computation Program!");
         System.out.print("Enter the number of companies: ");
@@ -22,46 +24,17 @@ public class Main {
 
             System.out.print("Enter Maximum Working Hours per Month: ");
             int maxWorkingHours = sc.nextInt();
-            computeEmployeeWage(companyName, wagePerHour, maxWorkingDays, maxWorkingHours);
+
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder(companyName, wagePerHour, maxWorkingDays, maxWorkingHours);
+            empWageBuilder.computeEmployeeWage();
+            companies.add(empWageBuilder);
+        }
+
+        System.out.println("\nSummary of Wages:");
+        for (EmpWageBuilder company : companies) {
+            System.out.println(company);
         }
 
         sc.close();
-    }
-
-    public static void computeEmployeeWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
-        int totalWorkingDays = 0;
-        int totalWorkingHours = 0;
-        int totalWage = 0;
-
-        System.out.println("\nCalculating wages for " + companyName + "...");
-        while (totalWorkingDays < maxWorkingDays && totalWorkingHours < maxWorkingHours) {
-            totalWorkingDays++;
-            int dailyHours = getWorkingHours();
-            totalWorkingHours += dailyHours;
-            if (totalWorkingHours > maxWorkingHours) {
-                totalWorkingHours -= dailyHours;
-                break;
-            }
-            int dailyWage = dailyHours * wagePerHour;
-            totalWage += dailyWage;
-
-            System.out.println("Day " + totalWorkingDays + ": Worked " + dailyHours + " hours, Earned: " + dailyWage);
-        }
-        System.out.println("Total Wage for " + companyName + ": " + totalWage);
-        System.out.println("Total Days Worked: " + totalWorkingDays + ", Total Hours Worked: " + totalWorkingHours);
-    }
-
-    private static int getWorkingHours() {
-        int empCheck = (int) (Math.random() * 3);
-        switch (empCheck) {
-            case 0: // Absent
-                return 0;
-            case 1: // Part-time
-                return 4;
-            case 2: // Full-time
-                return 8;
-            default:
-                return 0;
-        }
     }
 }
