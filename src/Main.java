2dde88d9935780437dcf1513464eb6c9453c9e10
shravnisingh <shravanisingh22@gmail.com>
2024-12-11 23:51:17 +0530

    UC4

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-        double employeeCheck = Math.random();
-        int wagePerHr = 20;
-        int empHrs, empWage;
-        if (employeeCheck > 0.6) {
-            System.out.println("Employee is doing FULL-TIME");
-            empHrs = 8;
-        } else if (employeeCheck > 0.3) {
-            System.out.println("Employee is doing PART-TIME");
-            empHrs = 4;
-        } else {
-            System.out.println("Employee is ABSENT");
-            empHrs = 0;
-        }
-        empWage = empHrs * wagePerHr;
-        System.out.println("Employee's Hours are: " + empHrs);
-        System.out.println("Employee's Wage is: " + empWage);
+        private  static final int isFullTime = 1;
+        private  static final int isPartTime = 2;
+        private  static final int wagePerHr = 20;
+        public static void main(String[] args) {
+            System.out.println("Welcome to Employee Wage Computation Program!");
+            int empHrs, empWage;
+            int employeeType = (int) Math.floor(Math.random() * 10) % 3;
+            switch (employeeType) {
+                case isFullTime:
+                    System.out.println("Employee is doing FULL-TIME");
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    System.out.println("Employee is doing PART-TIME");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is ABSENT");
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * wagePerHr;
+            System.out.println("Employee's Hours are: " + empHrs);
+            System.out.println("Employee's Wage is: " + empWage);
     }
 }
